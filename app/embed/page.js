8a86a792b4aa5dedd456826587bf48b45062f555
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-07-23 01:44:52 +0500

    Update page.js

--- app/embed/page.js
+++ app/embed/page.js
@@ -4,6 +4,17 @@ import React from 'react'
 import Header from '@/components/Header'
 
 const page = () => {
+
+  const [fileUrl, setFileUrl] = React.useState(null);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setFileUrl(url);
+      console.log(fileUrl)
+    }
+  };
+
   return (
     <div className="flex flex-col min-h-screen space-y-12 w-full scrollbar-container scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent" style={{ backgroundImage: 'url("https://media.istockphoto.com/id/657510172/vector/geometric-pattern-seamless-vector-background.jpg?s=612x612&w=0&k=20&c=IadUgpO6t724bMp6hVlxsy-qoAjIaWqp4qcrsJuHb8I=")'}}>
 
@@ -11,6 +22,10 @@ const page = () => {
 
         <div className='flex items-center justify-center text-red-900 font-bold text-9xl'>
           COMING SOON
+        </div>
+        <div className='flex items-center justify-center'>
+          <input type="file" onChange={handleFileChange} />
+          {fileUrl && <a href={fileUrl} target="_blank" rel="noopener noreferrer">Open File</a>}
         </div>
     </div>
   )
